56ff75ba48c3ff1383f10846fd462352333470e7
Jason Adams <jmadams41@gmail.com>
2022-04-28 16:43:18 -0600

    indexed variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+var timeBlockNine = document.getElementById("hr9am");
+var timeBlockTen = document.getElementById("hr10am");
+var timeBlockEleven = document.getElementById("hr11am");
+var timeBlockTwelve = document.getElementById("hr12pm");
+var timeBlockOne = document.getElementById("hr1pm");
+var timeBlockTwo = document.getElementById("hr2pm");
+var timeBlockThree = document.getElementById("hr3pm");
+var timeBlockFour = document.getElementById("hr4pm");
+var timeBlockFive = document.getElementById("hr5pm");
+
 $(".textarea").on("click", function() {
     var text= $(this)
     .text()
